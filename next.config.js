f9dd349f78518e5f09bbaca4f38f95f97b70cef4
“fanjingwenvi” <70184302+fanjingwenvi@users.noreply.github.com>
2025-11-03 22:40:51 +0100

    temp fix for the build size error

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const { withContentlayer } = require('next-contentlayer2')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Remove the experimental.appDir option as it's now stable in Next.js 14
+  // Exclude large photography images from file tracing (they're served statically)
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'public/photography/**/*.jpg',
+        'public/photography/**/*.jpeg',
+        'public/photography/**/*.png',
+      ],
+    },
+  },
 }
 
 module.exports = withContentlayer(nextConfig)
